66807993d02e92026fec844d2e836c7c949ae010
JeffC25 <jeff@yerf.dev>
2024-01-02 06:57:16 -0500

    construct absolute path for model cache location

--- server/app/routes/summary.py
+++ server/app/routes/summary.py
@@ -8,8 +8,8 @@ class SummaryRequest(BaseModel):
     text: str
 
 def summarize_text(text):
-    summarizer = pipeline("summarization", model="../../ml_models/")
-    summary = summarizer(text, max_length=130, min_length=30, do_sample=False)
+    summarizer = pipeline("summarization")
+    summary = summarizer(text)
     return summary[0]['summary_text']
 
 @router.post("/summary/")
