9d89ec8f2f4557fd60a909fe059964260a9e2a8b
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2024-01-25 11:44:39 -0500

    change checkbox toggles to <button> from <div>, add smooth color transitions

--- ui/src/components/CheckboxList.tsx
+++ ui/src/components/CheckboxList.tsx
@@ -24,13 +24,13 @@ const CheckboxList = ({ items, setItems }: Props) => {
         <div className="flex flex-col space-y-2 w-min">
             {items.map(item => (
                 <label key={item.id} className="flex items-center space-x-1">
-                    <div 
+                    <button 
                         onClick={() => handleCheck(item.id)}
-                        className={`w-12 h-6 rounded-full p-1 flex ${item.isChecked ? 'bg-lime-500' : 'bg-gray-300'}`}
+                        className={`w-12 h-6 rounded-full p-1 flex ${item.isChecked ? 'bg-lime-500' : 'bg-gray-300'} transition duration-300`}
                     >
                         <div className={`${item.isChecked ? 'w-full' : 'w-0'} duration-300`}></div>
                         <div className={`bg-white h-full aspect-square rounded-full`}></div>
-                    </div>
+                    </button>
                     <span className=" text-neutral-800">{item.value}</span>
                 </label>
             ))}
