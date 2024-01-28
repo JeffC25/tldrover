81e710100e5b394316ac897ced9f9e15e168948d
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2024-01-28 02:58:08 -0500

    specify path for huggingface and nltk data downloads, refactor polarity to sentiment with label and score, implment sentiment analysis and summarization, set up configs

--- server/app/routes/analyze.py
+++ server/app/routes/analyze.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter, HTTPException
 from transformers import pipeline
-from server.app.api.schemas import AnalysisRequest, AnalysisResponse
+from app.api.schemas import AnalysisRequest, AnalysisResponse, Sentiment
+from app.config import config
 
 router = APIRouter()
 
 def summarize_text(text):
-    summarizer = pipeline("summarization")
+    summarizer = pipeline("summarization", model=config['summarization_model'])
     summary = summarizer(text)
     return summary[0]['summary_text']
 
@@ -14,24 +15,24 @@ def extract_keywords(text):
     return ["test1", "test2", "test3"]
 
 def analyze_sentiment(text):
-    sentiment_analyzer = pipeline("sentiment-analysis")
+    sentiment_analyzer = pipeline("sentiment-analysis", model=config['sentiment_analysis_model'])
     result = sentiment_analyzer(text)
-    return result[0]
+    return Sentiment(label=result[0]['label'], score=result[0]['score'])
 
 @router.post("/analyze/", response_model=AnalysisResponse)
 async def analysis_route(request: AnalysisRequest):
-    if not (request.summary or request.keywords or request.polarity):
+    if not (request.summary or request.keywords or request.sentiment):
         raise HTTPException(
             status_code=400, 
-            detail="At least one of 'summary', 'keywords', or 'polarity' must be set to true."
+            detail="At least one of 'summary', 'keywords', or 'sentiment' must be set to true."
         )
 
     try:
         summary = summarize_text(request.text) if request.summary else None
         keywords = extract_keywords(request.text) if request.keywords else None
-        polarity = analyze_sentiment(request.text) if request.polarity else None
+        sentiment = analyze_sentiment(request.text) if request.sentiment else None
 
-        return AnalysisResponse(summary=summary, keywords=keywords, polarity=polarity)
+        return AnalysisResponse(summary=summary, keywords=keywords, sentiment=sentiment)
 
     except Exception as e:
         raise HTTPException(
