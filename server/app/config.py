9fa6c0cdfaacf4d87b969f3fecc3d9d696efae79
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2024-01-29 01:21:18 -0500

    handle long inputs for sentiment analysis and summarization, set CORS

--- server/app/config.py
+++ server/app/config.py
@@ -1,4 +1,3 @@
-
 import yaml
 from pathlib import Path
 
--- server/app/routes/analyze.py
+++ server/app/routes/analyze.py
@@ -6,9 +6,31 @@ from app.config import config
 router = APIRouter()
 
 def summarize_text(text):
+    tokenizer = AutoTokenizer.from_pretrained(config['summarization_model'])
     summarizer = pipeline("summarization", model=config['summarization_model'])
-    summary = summarizer(text)
-    return summary[0]['summary_text']
+
+    # Split the text into chunks that fit within the model's maximum token limit
+    max_length = 1024  # Adjust as needed based on the model's limit
+    tokens = tokenizer(text, truncation=False, return_tensors="pt", padding=False)
+    total_tokens = tokens.input_ids.size(1)
+
+    # If the text is shorter than the max_length, summarize it directly
+    if total_tokens <= max_length:
+        summary = summarizer(text, max_length=150, min_length=30, truncation=True)
+        return summary[0]['summary_text'].replace(' .', '.')
+
+    # If the text is longer, split it and summarize each part
+    summarized_text = ''
+    start = 0
+    while start < total_tokens:
+        end = start + max_length
+        chunk = tokenizer.decode(tokens.input_ids[0][start:end], skip_special_tokens=True)
+        summary = summarizer(chunk, max_length=150, min_length=30, truncation=True)
+        summarized_text += summary[0]['summary_text'].replace(' .', '.') + ' '
+        start = end
+
+    return summarized_text
+    
 
 def extract_keywords(text):
     tokenizer = AutoTokenizer.from_pretrained(config['keyword_extraction_model'])
@@ -23,11 +45,10 @@ def extract_keywords(text):
     unique = set()
     for word in filtered:
         unique.add(word['word'])
-
     return(list(unique))
 
 def analyze_sentiment(text):
-    sentiment_analyzer = pipeline("sentiment-analysis", model=config['sentiment_analysis_model'])
+    sentiment_analyzer = pipeline("sentiment-analysis", model=config['sentiment_analysis_model'], max_length=512)
     result = sentiment_analyzer(text)
     return Sentiment(label=result[0]['label'], score=result[0]['score'])
 
