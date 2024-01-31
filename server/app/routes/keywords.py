2f6ac32f993b8c4d85b5296631c0bca55441af61
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2024-01-31 18:15:10 -0500

    split /analyze/ request into /summary/, /keywords/, and /sentiment/ requests

--- server/app/routes/keywords.py
+++ server/app/routes/keywords.py
@@ -1,20 +1,29 @@
-# from fastapi import APIRouter, HTTPException
-# from pydantic import BaseModel
-# from transformers import pipeline
+from fastapi import APIRouter, HTTPException
+from transformers import pipeline, AutoTokenizer, AutoModelForTokenClassification
+from app.api.schemas import KeywordsRequest, KeywordsResponse
+from app.config import config
 
-# router = APIRouter()
+router = APIRouter()
 
-# class KeywordsRequest(BaseModel):
-#     text: str
+def extract_keywords(text, minScore=0.9):
+    tokenizer = AutoTokenizer.from_pretrained(config['keyword_extraction_model'])
+    model = AutoModelForTokenClassification.from_pretrained(config['keyword_extraction_model'])
+    keyword_extractor = pipeline("ner", model=model, tokenizer=tokenizer, aggregation_strategy="simple")
 
-# def extract_keywords(text):
-#     # Placeholder 
-#     return ["test1", "test2", "test3"]
+    # extract keywords and filter out low scores
+    extracted = keyword_extractor(text) 
+    filtered = [word for word in extracted if word['score'] >= minScore]
 
-# @router.post("/keywords/")
-# async def extract_keywords_route(request: KeywordsRequest):
-#     try:
-#         keywords = extract_keywords(request.text)
-#         return {"keywords": keywords}
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e))
+    # return only unique keywords
+    unique = set()
+    for word in filtered:
+        unique.add(word['word'])
+    return(list(unique))
+
+@router.post("/keywords/")
+async def extract_keywords_route(request: KeywordsRequest):
+    try:
+        keywords = extract_keywords(request.text, request.minScore)
+        return KeywordsResponse(keywords=keywords)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
