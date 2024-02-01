77ccbd3a087c7575708f951f5aabc54e7c1f683f
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2024-02-01 09:13:15 -0500

    move logic for setting download location from main.py to init.py, remove mid-file imports

---
+++ server/app/__init__.py
@@ -0,0 +1,24 @@
+# Resolve paths relative to the script file
+from app.config import config
+from pathlib import Path
+import nltk
+import os
+
+script_dir = Path(__file__).resolve().parent
+hf_home_dir = script_dir / config['hf_home']
+nltk_data_dir = script_dir / config['nltk_data']
+
+# Set the environment variables for transformers and nltk data locations
+os.environ["HF_HOME"] = str(hf_home_dir.resolve())
+os.environ["NLTK_DATA"] = str(nltk_data_dir.resolve())
+
+# Ensure the directories exist
+hf_home_dir.mkdir(parents=True, exist_ok=True)
+nltk_data_dir.mkdir(parents=True, exist_ok=True)
+
+# Download nltk data if it's not already present
+try:
+    nltk.data.path.append(str(nltk_data_dir.resolve()))
+    _ = nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt', download_dir=os.environ["NLTK_DATA"])
